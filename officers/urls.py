733864049bddfa34193b9a15c6dd55c0ecff132d
REAL NAME <EMAIL_USED_ON_GITHUB>
2026-03-25 02:01:33 -0700

    feat(officers): add officer profile URL route to officers module

--- officers/urls.py
+++ officers/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'officers'
 
 urlpatterns = [
+    path('profile/', views.officer_profile, name='officer_profile'),
     path('applications/', views.officer_application_list, name='application_list'),
     path('applications/all/', views.officer_application_all, name='application_all'),
     path('applications/<int:pk>/', views.officer_application_detail, name='application_detail'),
